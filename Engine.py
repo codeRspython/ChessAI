fa1263afc1ff5db3fa3be3d6a17dc8bbdc0357bc
codeRSpython <simon.n.brunnauer@gmail.com>
2022-06-20 18:27:10 +0200

    Fixed another bug regarding check

--- Engine.py
+++ Engine.py
@@ -2,7 +2,6 @@ import numpy as np
 
 class GameState():
     def __init__(self):
-        # Initialize the beginning state of a board
         self.board = [
             ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
             ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
@@ -14,19 +13,15 @@ class GameState():
             ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
             ]
 
-        # Initializing a dictionary where the move function of a piece can be accessed by typing the symbol for the piece
         self.moveFunctions = {'p': self.getPawnMoves, 'N': self.getKnightMoves, 'B': self.getBishopMoves, 'R': self.getRookMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
-        # Initializing whose move it is in the beginning
         self.whiteToMove = True
         self.moveLog = []
-        # Initializing the location of both kings in order to check for checks
         self.whiteKingLocation = (7, 4)
         self.blackKingLocation = (0, 4)
+        self.enpassantSquare = ()
 
     def makeMove(self, move):
-        # Setting the square of the piece that moved empty
         self.board[move.startRow][move.startCol] = "--"
-        # Moving the piece
         self.board[move.endRow][move.endCol] = move.pieceMoved
         self.moveLog.append(move)
         # Update the location of the king
@@ -36,41 +31,45 @@ class GameState():
             self.blackKingLocation = (move.endRow, move.endCol)
         # Pawn Promotion
         if move.isPawnPromotion:
-            color = 'w' if self.whiteToMove else 'b'
-            self.board[move.endRow][move.endCol] = color + move.promotionChoice
+            self.board[move.endRow][move.endCol] = move.pieceMoved[0] + move.promotionChoice
+        # En Passant
+        if move.isEnPassant:
+            self.board[move.startRow][move.endCol] = '--'
+        if move.pieceMoved[1] == 'p' and abs(move.endRow - move.startRow) == 2:
+            self.enpassantSquare = ((move.startRow + move.endRow)//2, move.endCol)
+        else:
+            self.enpassantSquare = ()
         # Switching turns
         self.whiteToMove = not self.whiteToMove
 
-
     def undoMove(self):
-        # Checking if there is a move to undo
         if len(self.moveLog) != 0:
-            # Removing the last move
             move = self.moveLog.pop()
-            # Reversing the squares
             self.board[move.startRow][move.startCol] = move.pieceMoved
             self.board[move.endRow][move.endCol] = move.pieceCaptured
-            # Reversing the turns
             self.whiteToMove = not self.whiteToMove
             # Updating the Kings' location
             if move.pieceMoved == 'wK':
                 self.whiteKingLocation = (move.startRow, move.startCol)
             elif move.pieceMoved == 'bK':
                 self.blackKingLocation = (move.startRow, move.startCol)
+            # En Passant
+            if move.isEnPassant:
+                self.board[move.endRow][move.endCol] = '--'
+                self.board[move.startRow][move.endCol] = move.pieceCaptured
+                move.enpassantSquare = (move.endRow, move.endCol)
+            if move.pieceMoved[1] == 'p' and abs(move.startRow - move.endRow) == 2:
+                move.enpassantSquare = ()
+
 
     def getValidMoves(self):
-        # Initializing all the directions a king might get attacked
+        tempEnpassantSquare = self.enpassantSquare
         directions = ((-1, -1), (-1, 1), (1, -1), (1, 1), (-1, 0), (1, 0), (0, -1), (0, 1))
         knightDirections = ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1))
-        # Initializing a list of all the pseudolegal moves black can make
         moves = self.getPseudolegalMoves()
-        # Initializing an empty list, which will contain only the valid moves in the end
         validMoves = []
-        # Iterating through the list of blacks' moves (backwards, since we are removing items from it)
         for j in range(len(moves)-1, -1, -1):
-            # Making the move --> now self.whiteToMove = True
             self.makeMove(moves[j])
-            # Initializing the location of the king, the color of the enemy/ally and the directions from which a pawn can attack depending on which turn it is currently
             if not self.whiteToMove:
                 startRow = self.whiteKingLocation[0]
                 startCol = self.whiteKingLocation[1]
@@ -83,33 +82,28 @@ class GameState():
                 enemyColor = 'w'
                 allyColor = 'b'
                 pawnDirections = [0, 1]
-            # Iterating through all the directions
             for d in range(len(directions)):
-                # Iterating through each possible square in this direction
                 for i in range(1,8):
                     endRow = startRow + directions[d][0] * i
                     endCol = startCol + directions[d][1] * i
-                    # Making sure the square is still on the board and not out of bounds
                     if 0 <= endRow < 8 and 0 <= endCol < 8:
                         endPiece = self.board[endRow][endCol]
                         color = endPiece[0]
                         type = endPiece[1]
-                        # Checking if there is an enemy piece on the square that's not a king (since a king is never a danger to another king)
                         if color == enemyColor and type != 'K':
-                            # If an enemy piece is on the square check if it's attacking the king from a direction it is really able to attack the king
                             if (0 <= d <= 3 and (type == 'B' or type == 'Q')) or \
                                 (4 <= d <= 7 and (type == 'R' or type == 'Q')) or \
                                     ((d == pawnDirections[0] or d == pawnDirections[1]) and i == 1 and type == 'p'):
-                                    # If after the move is made, a piece is actually attacking the king, remove this move as it has to be unvalid
                                     moves.remove(moves[j])
                                     break
+                            else:
+                                break
                         elif color == allyColor:
                             break
                         else:
                             pass
                     else:
                         pass
-            # Iterating through all the directions a knight might be able to attack the king
             for d in range(len(knightDirections)):
                 endRow = startRow + knightDirections[d][0]
                 endCol = startCol + knightDirections[d][1]
@@ -119,9 +113,8 @@ class GameState():
                         type = endPiece[1]
                         if color == enemyColor and type == 'N':
                             moves.remove(moves[j])
-            # Undoing the move in order to not mess up the current gamestate
             self.undoMove()
-        # After all the unvalid moves have been removed, the validMoves list only contains the remaining moves, that are in fact valid
+        self.enpassantSquare = tempEnpassantSquare
         validMoves = moves
         print('Legal moves: ' + str(len(moves)))
         return validMoves
@@ -133,15 +126,10 @@ class GameState():
             return self.squareUnderAttack(self.blackKingLocation[0], self.blackKingLocation[1])
 
     def squareUnderAttack(self, r, c):
-        # Switching turns in order to generate the opponents' moves
         self.whiteToMove = not self.whiteToMove
-        # Generating the opponents' moves
         oppMoves = self.getValidMoves()
-        # Switching back the turns
         self.whiteToMove = not self.whiteToMove
-        # Iterating through each opponents' move
         for move in oppMoves:
-            # If the endsquare of the move has the given coordinates, the given square is being attacked
             if move.endRow == r and move.endCol == c:
                 return True
         return False
@@ -166,9 +154,13 @@ class GameState():
             if c-1 >= 0:
                 if self.board[r-1][c-1][0] == 'b':
                     moves.append(Move((r,c), (r-1,c-1), self.board))
+                elif (r-1, c-1) == self.enpassantSquare:
+                    moves.append(Move((r,c), (r-1,c-1), self.board, isEnPassant=True))
             if c+1 <= 7:
                 if self.board[r-1][c+1][0] == 'b':
                     moves.append(Move((r,c), (r-1,c+1), self.board))
+                elif (r-1, c+1) == self.enpassantSquare:
+                    moves.append(Move((r,c), (r-1,c+1), self.board, isEnPassant=True))
         else:
             if self.board[r+1][c] == '--':
                 moves.append(Move((r,c), (r+1, c), self.board))
@@ -177,9 +169,13 @@ class GameState():
             if c-1 >= 0:
                 if self.board[r+1][c-1][0] == 'w':
                     moves.append(Move((r,c), (r+1, c-1), self.board))
+                elif (r+1, c-1) == self.enpassantSquare:
+                    moves.append(Move((r,c), (r+1,c-1), self.board, isEnPassant=True))
             if c+1 <= 7:
                 if self.board[r+1][c+1][0] == 'w':
                     moves.append(Move((r,c), (r+1, c+1), self.board))
+                elif (r+1, c+1) == self.enpassantSquare:
+                    moves.append(Move((r,c), (r+1,c+1), self.board, isEnPassant=True))
 
     def getKnightMoves(self, r, c, moves):
         knightMoves = ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1))
@@ -254,17 +250,18 @@ class Move():
     filesToCols = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
     colsToFiles = {v: k for k, v in filesToCols.items()}
 
-    def __init__(self, startSq, endSq, board):
+    def __init__(self, startSq, endSq, board, isEnPassant=False):
         self.startRow = startSq[0]
         self.startCol = startSq[1]
         self.endRow = endSq[0]
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
-        self.isPawnPromotion = False
-        if (self.pieceMoved == 'wp' and self.endRow == 0) or (self.pieceMoved == 'bp' and self.endRow == 7):
-            self.isPawnPromotion = True
+        # Pawn promotion
+        self.isPawnPromotion = (self.pieceMoved == 'wp' and self.endRow == 0) or (self.pieceMoved == 'bp' and self.endRow == 7)
         self.promotionChoice = 'Q'
+        # En passant
+        self.isEnPassant = isEnPassant
         self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
 
     def __eq__(self, other):
--- README.txt
+++ README.txt
@@ -31,3 +31,45 @@ Legal moves for black:
 27th move: bQ (0,3) --> (1,4) = NOT REMOVED SUCCESSFULLY
 28th move: bN (0,1) --> (2,2) = REMOVED SUCCESSFULLY
 29th move: bN (0,1) --> (2,0) = REMOVED SUCCESSFULLY
+
+
+Debug scenario: Double Bishop Check
+Gamestate: self.board = [
+            ['bR', 'bN', 'bB', 'bQ', '--', 'bB', 'bN', 'bR'],
+            ['bp', 'bp', '--', 'bp', 'bK', 'bp', 'bp', '--'],
+            ['--', '--', 'bp', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', 'bp', '--', '--', 'bp'],
+            ['--', 'wB', 'wB', '--', 'wp', '--', '--', '--'],
+            ['--', '--', '--', 'wp', '--', '--', '--', '--'],
+            ['wp', 'wp', 'wp', '--', '--', 'wp', 'wp', 'wp'],
+            ['wR', 'wN', '--', 'wQ', 'wK', '--', 'wN', 'wR']
+            ]
+self.whiteToMove = False
+Pseudolegal moves for black: 25
+Legal moves for black:
+
+1st move: bp (3,7) --> (4,7) = REMOVED SUCCESSFULLY
+2nd move: bp (2,2) --> (3,2) = NOT REMOVED SUCCESSFULLY
+3rd move: bp (1,6) --> (3,6) = REMOVED SUCCESSFULLY
+4th move: bp (1,6) --> (2,6) = REMOVED SUCCESSFULLY
+5th move: bp (1,5) --> (3,5) = REMOVED SUCCESSFULLY
+6th move: bp (1,5) --> (2,5) = REMOVED SUCCESSFULLY
+7th move: bK (1,4) --> (2,4) = REMOVED SUCCESSFULLY
+8th move: bK (1,4) --> (0,4) = NOT REMOVED SUCCESSFULLY
+9th move: bK (1,4) --> (2,5) = NOT REMOVED SUCCESSFULLY
+10th move: bK (1,4) --> (2,3) = REMOVED SUCCESSFULLY
+11th move: bp (1,3) --> (3,3) = REMOVED SUCCESSFULLY
+12th move: bp (1,3) --> (2,3) = NOT REMOVED SUCCESSFULLY
+13th move: bp (1,1) --> (3,1) = REMOVED SUCCESSFULLY
+14th move: bp (1,1) --> (2,1) = REMOVED SUCCESSFULLY
+15th move: bp (1,0) --> (3,0) = REMOVED SUCCESSFULLY
+16th move: bp (1,0) --> (2,0) = REMOVED SUCCESSFULLY
+17th move: bR (0,7) --> (2,7) = REMOVED SUCCESSFULLY
+18th move: bR (0,7) --> (1,7) = REMOVED SUCCESSFULLY
+19th move: bN (0,6) --> (2,7) = REMOVED SUCCESSFULLY
+20th move: bN (0,6) --> (2,5) = REMOVED SUCCESSFULLY
+21st move: bQ (0,3) --> (0,4) = REMOVED SUCCESSFULLY
+22nd move: bQ (0,3) --> (3,0) = REMOVED SUCCESSFULLY
+23rd move: bQ (0,3) --> (2,1) = REMOVED SUCCESSFULLY
+24th move: bQ (0,3) --> (1,2) = REMOVED SUCCESSFULLY
+25th move: bN (0,1) --> (2,0) = REMOVED SUCCESSFULLY
